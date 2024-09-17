ae4b19adaec01b10797deec491395e9622c3f19d
jwetherbee2004 <jared.wetherbee04@gmail.com>
2024-09-17 18:02:37 -0400

    Lab REC

---
+++ Resident Evil Clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float mouseSensitivity = 60f;
+    [SerializeField] Transform fpsCamera;
+
+    [SerializeField] float verticalLookLimit = 85f;
+
+    private bool isGrounded;
+    private float xRotation = 0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        LookAround();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * moveX + transform.forward * moveZ).normalized;
+
+        Vector3 moveVelocity = move * moveSpeed;
+        moveVelocity.y = rb.velocity.y;
+
+        rb.velocity = moveVelocity;
+    }
+
+    private void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -verticalLookLimit, verticalLookLimit);
+
+        fpsCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void Jump()
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
